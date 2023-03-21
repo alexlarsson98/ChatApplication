c9760d2fce31f4a633d89ac6ced86e4369ce8ed1
Alexander Larsson <106491522+assistans-alexander@users.noreply.github.com>
2023-03-21 08:13:05 +0100

    add initial dockerized frontend

---
+++ client/chat-app-frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <input type="text"/>
+        <button>Start chat</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
